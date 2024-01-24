7d8d8f3c0fb6af79501aeec2d229004813dcf8b7
nnoviiftr <novianifitrinabilah@gmail.com>
2024-01-24 16:44:28 +0700

    modified:   __init__.py 	modified:   models/doctors.py 	modified:   views/appointments_view.xml 	modified:   views/doctors_view.xml 	modified:   views/patients_view.xml

--- models/doctors.py
+++ models/doctors.py
@@ -5,5 +5,26 @@ class DentalDoctor(models.Model):
     _description = 'Dental Doctor'
 
     doctor = fields.Many2one('res.partner', string="Doctor", required=True)
-    image = fields.Binary(string="image", attachment=True)
+    image = fields.Binary(string=".", attachment=True)
     service_id = fields.Many2one('dental.service', string="Service")
+    stage = fields.Selection([
+        ('work', 'Work'),
+        ('leave', 'Leave'),
+    ], default='work', string='Status', required=True)
+ 
+    def action_work(self):
+        self.stage = 'work'
+        
+    def action_leave(self):
+        self.stage = 'leave'
+        
+    list_visible = fields.Boolean(compute='_compute_list_visible', store=True)
+    def _compute_list_visible(self):
+        for doctor in self:
+            doctor.list_visible = doctor.stage != 'leave'
+
+    def action_work(self):
+        self.stage = 'work'
+        
+    def action_leave(self):
+        self.stage = 'leave'
